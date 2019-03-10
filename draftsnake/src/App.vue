57dcad28f0d760c76a252e2812215fc046b45501
DESKTOP-CTN1NPE\brian <briandfrederick@gmail.com>
2019-03-10 08:47:35 -0500

    add draft pick form

--- draftsnake/src/App.vue
+++ draftsnake/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DraftPickForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DraftPickForm from './components/DraftPickForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    DraftPickForm
   }
 }
 </script>
---
+++ draftsnake/src/components/DraftPickForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div v-if="onTheClock">
+      <input v-model="draftPick"/>
+      <button v-on:click="submitPick">Make your pick</button>
+    </div>
+    <ul>
+      <li v-for="item in draftPicks" :key="item.Id">{{ item.Id }} {{item.User}} {{item.Pick}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DraftPickForm',
+  methods: {
+    submitPick: function(){
+      this.draftPicks.push({
+        User: 'user', 
+        Id: this.rank,
+        Pick: this.draftPick
+      });
+      this.draftPick = '';
+      this.rank++;
+      this.onTheClock = false;
+    }
+  },
+  data: function() {
+    return {
+      onTheClock: true,
+      rank: 1,
+      draftPick: '',
+      draftPicks: []
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
